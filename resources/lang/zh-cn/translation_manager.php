96204a5b1c3052aefefec45d1dbfe2c417fa3a45
OPDLNF <opdlnf01@gmail.com>
2025-08-24 09:07:04 +0700

    update: new lang

---
+++ resources/lang/zh-cn/translation_manager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'translation_line' => '翻译行',
+    'translation_lines' => '翻译行列表',
+
+    'group' => '组',
+    'key' => '键',
+    'text' => '文本',
+
+    'created' => '已创建',
+    'updated' => '已更新',
+
+    'source' => '来源',
+    'database' => '数据库',
+    'file' => '文件',
+
+    'show_all_languages' => '显示所有语言',
+    'revert' => '还原',
+
+    'revert_confirm' => '您确定要还原此翻译吗？',
+    'revert_confirmation_title' => '翻译已还原',
+    'revert_confirmation_message' => '翻译已成功还原',
+    'revert_confirmation_not_title' => '翻译<strong>未</strong>还原',
+    'revert_confirmation_not_message' => '发生错误。您的翻译可能未被还原。',
+
+    'validation_missing_languages' => '缺少翻译语言键。',
+    'validation_missing_group' => '缺少翻译组键。',
+    'validation_missing_key' => '缺少翻译键。',
+];
